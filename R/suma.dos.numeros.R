7c4d1dcfd00e476059b503ab51416e592c327641
hectorfr1984 <hector@MacBook-Air-de-Hector.local>
2017-01-04 17:02:21 +0100

    PracticaR
    Ejercicio R

---
+++ R/suma.dos.numeros.R
@@ -0,0 +1,16 @@
+#' Sumatorio de dos numeros.
+#' 
+#' @param x un dato numerico.
+#' @param y un dato numerico.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' suma.dos.numeros(12, 3)
+#' suma.dos.numeros(10.8, 1.3)
+
+suma.dos.numeros <- function(x, y) {
+  x + y
+}
+
+
+
+
